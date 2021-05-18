ea84006bf59a68408deadf88228e2aa143148409
Sahana Sivanandarajah <81416603+sahanas1998@users.noreply.github.com>
2021-05-18 23:40:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+  var n1=Number(req.body.num1);
+  var n2=Number(req.body.num2);
+  //console.log(req.body);
+  var result=n1+n2;
+  res.send("the result is :"+result);
+});
+
+
+app.listen(3000,function(){
+  console.log("Server has started on port 3000");
+});
